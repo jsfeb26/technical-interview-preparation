30235ecb179d52530ceeea1f1ad906a9bf9961ac
jsfeb26 <jsfeb26@gmail.com>
2015-12-05 23:07:42 -0500

    Wildcard solution and tests

---
+++ Algorithms/wildcard.js
@@ -0,0 +1,37 @@
+// '?' is a wildcard
+// Input: 10?
+// Output: 101 100
+//
+// Input: 1?0?
+// Output: 1000 1001 1100 1101
+function calculateWildcard(strArray, result) {
+  var wildCardIndex = strArray.indexOf('?');
+
+  // if no more wildCards then add to result array
+  if (wildCardIndex < 0) {
+    result.push(strArray.join(''));
+  }
+  else {
+    // replace wildcard with 0
+    strArray[wildCardIndex] = 0;
+    calculateWildcard(strArray, result);
+
+    // replace wildcard with 1
+    strArray[wildCardIndex] = 1;
+    calculateWildcard(strArray, result);
+
+    // put wildcard card back to ?
+    strArray[wildCardIndex] = '?';
+  }
+}
+
+export default function wildcard(str) {
+  if (str.indexOf('?') < 0) {
+    return str;
+  }
+  else {
+    var result = [];
+    calculateWildcard(str.split(''), result);
+    return result.join(' ');
+  }
+}
--- test/recursion_test.js
+++ test/recursion_test.js
@@ -1,5 +1,6 @@
 import {expect} from 'chai';
 import bestPath from '../Algorithms/bestPath';
+import wildcard from '../Algorithms/wildcard';
 
 describe('Best Path Algorithm', () => {
   it('Basic Best Path correct test', () => {
@@ -16,5 +17,25 @@ describe('Best Path Algorithm', () => {
     const result = bestPath(board);
 
     expect(result).to.equal(1 + 9 + 8 + 7 + 9 + 8 + 2);
+  });
+});
+
+describe('Wildcard Algorithm', () => {
+  it('10? Correct Solution', () => {
+    var result = wildcard('10?');
+
+    expect(result).to.equal('100 101');
+  });
+
+  it('1?0? Correct Solution', () => {
+    var result = wildcard('1?0?');
+
+    expect(result).to.equal('1000 1001 1100 1101');
+  });
+
+  it('Edge case 10', () => {
+    var result = wildcard('10');
+
+    expect(result).to.equal('10');
   });
 });

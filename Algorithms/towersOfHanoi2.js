7446f47e7f2cda193b788effd35e054c681a6787
jsfeb26 <jsfeb26@gmail.com>
2015-12-05 20:26:27 -0500

    Add towers of hanoi variation

---
+++ Algorithms/towersOfHanoi2.js
@@ -0,0 +1,39 @@
+function towersOfHanoi(n) {
+  //T(n) = O(2^n)
+  // S(n) = O(n)
+  var towers = {
+    'A': n,
+    'B': 0,
+    'C': 0,
+    print: function print() {
+      console.log('');
+      console.log('Tower A has ' + this['A'] + ' discs');
+      console.log('Tower B has ' + this['B'] + ' discs');
+      console.log('Tower C has ' + this['C'] + ' discs');
+    }
+  }
+
+  move(n, towers, 'A', 'C', 'B');
+}
+
+function move(n, towers, from, to, inter) {
+  if (n === 1) {
+    moveAction(towers, from, to);
+  }
+  else {
+    // move to the inter from before using the to from before
+    move(n - 1, towers, from, inter, to);
+    moveAction(towers, from, to);
+
+    // move from inter to the original destination
+    move(n - 1, towers, inter, to, from);
+  }
+}
+
+function moveAction(towers, from, to) {
+  towers[from] -= 1;
+  towers[to] += 1;
+  towers.print();
+}
+
+towersOfHanoi(3);
